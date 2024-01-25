0a1da4b322bea50ec2e675d7b23805536a62cd01
valetra <valetra100102@gmail.com>
2024-01-26 01:48:11 +0300

    replace method

--- API/Services/SupplyService/ISupplyService.cs
+++ API/Services/SupplyService/ISupplyService.cs
@@ -5,6 +5,6 @@ namespace Services;
 public interface ISupplyService
 {
     Task<List<Supply>> GetAll();
-    Task<bool> Delete(Guid id);
     Task<bool> Create(Supply supply);
+    Task<bool> Delete(Guid id);
 }
