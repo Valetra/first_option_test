857c80f70c95adead8604f1243d5a09832269db8
valetra <valetra100102@gmail.com>
2024-01-26 00:31:25 +0300

    1)add context, repository, service of supply; 2) add connection string;

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+using Services;
+using DAL.Contexts;
+using DAL.Repositories;
+using DAL.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+string? databaseConnectionString = configuration.GetConnectionString("database");
+
+if (databaseConnectionString is null)
+{
+    string message = "Connection string \"database\" is required";
+    Console.Error.WriteLine(message);
+    return;
+}
+
+builder.Services.AddDbContext<SupplyContext>(options => options.UseNpgsql(databaseConnectionString));
+
+builder.Services.AddScoped<IBaseRepository<Supply, Guid>, BaseRepository<Supply, Guid>>(serviceProvider =>
+    new BaseRepository<Supply, Guid>(serviceProvider.GetRequiredService<SupplyContext>()));
+
+builder.Services.AddScoped<ISupplyService, SupplyService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
