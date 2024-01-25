acad49ad076af2471eda4d4ccf98629fcb57e7ac
valetra <valetra100102@gmail.com>
2024-01-26 01:37:13 +0300

    add init values

--- API/DAL/Orders/Model/Order.cs
+++ API/DAL/Orders/Model/Order.cs
@@ -5,8 +5,8 @@ namespace DAL.Models;
 public class Order : BaseModel<Guid>
 {
     public int Number { get; set; }
-    public string Status { get; set; }
+    public string Status { get; set; } = "";
     public DateTime CreateDateTime { get; set; } = DateTime.UtcNow;
-    public List<Guid> Supplies { get; set; }
+    public List<Guid> Supplies { get; set; } = new List<Guid>();
     public int OrderCost { get; set; }
 }
