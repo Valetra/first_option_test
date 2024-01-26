c833df05926cd77c8d3ad0e18c307d81d2d09b55
valetra <valetra100102@gmail.com>
2024-01-26 14:07:55 +0300

    replace required attribute

--- API/DTO/RequestObjects/Supply.cs
+++ API/DTO/RequestObjects/Supply.cs
@@ -8,7 +8,7 @@ public class Supply
     [MaxLength(20, ErrorMessage = "Name cannot exceed 20 characters")]
     public required string Name { get; set; }
 
-    [Range(1, int.MaxValue, ErrorMessage = "The field Cost must be greater than or equal 1")]
     [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "The field Cost must be greater than or equal 1")]
     public required string Cost { get; set; }
 }
